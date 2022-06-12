417f458c5f8426f0368721443d6c5a08aa65fa2f
Tomas Kala <me@tomaskala.com>
2022-06-12 17:13:34 +0200

    Add a stub scanner

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -8,4 +8,16 @@ void
 compile(const char *source)
 {
   scanner_init(source);
+  int line = -1;
+  for (;;) {
+    Token token = scanner_scan_token();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else
+      printf(" | ");
+    printf("%2d '%.*s'\n", token.type, (int) token.length, token.start);
+    if (token.type == TOKEN_EOF)
+      break;
+  }
 }
--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -19,3 +19,41 @@ scanner_init(const char *source)
   scanner.current = source;
   scanner.line = 1;
 }
+
+static bool
+is_at_end()
+{
+  return *scanner.current == '\0';
+}
+
+static Token
+make_token(TokenType type)
+{
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = scanner.current - scanner.start;
+  token.line = scanner.line;
+  return token;
+}
+
+static Token
+error_token(const char *message)
+{
+  Token token;
+  token.type = TOKEN_ERROR;
+  token.start = message;
+  token.length = strlen(message);
+  token.line = scanner.line;
+  return token;
+}
+
+Token
+scanner_scan_token()
+{
+  scanner.start = scanner.current;
+  if (is_at_end())
+    return make_token(TOKEN_EOF);
+  else
+    return error_token("Unexpected character.");
+}
