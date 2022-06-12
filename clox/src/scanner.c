be62ad1fb86617e80d3940ed0e799dfccb34fa39
Tomas Kala <me@tomaskala.com>
2022-06-12 17:29:25 +0200

    Match simple tokens

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -26,6 +26,30 @@ is_at_end()
   return *scanner.current == '\0';
 }
 
+static char
+advance()
+{
+  scanner.current++;
+  return scanner.current[-1];
+};
+
+static char
+peek()
+{
+  return *scanner.current;
+}
+
+static bool
+match(char expected)
+{
+  if (is_at_end())
+    return false;
+  else if (*scanner.current != expected)
+    return false;
+  scanner.current++;
+  return true;
+}
+
 static Token
 make_token(TokenType type)
 {
@@ -48,12 +72,51 @@ error_token(const char *message)
   return token;
 }
 
+static void
+skip_whitespace()
+{
+  for (;;) {
+    char c = peek();
+    switch (c) {
+    case ' ':
+    case '\r':
+    case '\t':
+      advance();
+      break;
+    case '\n':
+      scanner.line++;
+      advance();
+      break;
+    default:
+      return;
+    }
+  }
+}
+
 Token
 scanner_scan_token()
 {
+  skip_whitespace();
   scanner.start = scanner.current;
   if (is_at_end())
     return make_token(TOKEN_EOF);
-  else
-    return error_token("Unexpected character.");
+  char c = advance();
+  switch (c) {
+  case '(': return make_token(TOKEN_LEFT_PAREN);
+  case ')': return make_token(TOKEN_RIGHT_PAREN);
+  case '{': return make_token(TOKEN_LEFT_BRACE);
+  case '}': return make_token(TOKEN_RIGHT_BRACE);
+  case ';': return make_token(TOKEN_SEMICOLON);
+  case ',': return make_token(TOKEN_COMMA);
+  case '.': return make_token(TOKEN_DOT);
+  case '-': return make_token(TOKEN_MINUS);
+  case '+': return make_token(TOKEN_PLUS);
+  case '/': return make_token(TOKEN_SLASH);
+  case '*': return make_token(TOKEN_STAR);
+  case '!': return make_token(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+  case '=': return make_token(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+  case '<': return make_token(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '>': return make_token(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  }
+  return error_token("Unexpected character.");
 }
