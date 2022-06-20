7e16e9143119480e39a4b8c27128ca7e525e065e
Tomas Kala <me@tomaskala.com>
2022-06-20 18:27:02 +0200

    Add support for object types

---
+++ clox/src/object.h
@@ -0,0 +1,33 @@
+#ifndef CLOX_OBJECT_H
+#define CLOX_OBJECT_H
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+#define IS_STRING(value) is_obj_type(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString *) AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString *) AS_OBJ(value))->chars)
+
+typedef enum {
+  OBJ_STRING
+} ObjType;
+
+struct Obj {
+  ObjType type;
+};
+
+struct ObjString {
+  Obj obj;
+  size_t length;
+  char *chars;
+};
+
+static inline bool
+is_obj_type(Value value, ObjType type)
+{
+  return IS_OBJ(value) && OBJ_TYPE(value) == type;
+}
+
+#endif
